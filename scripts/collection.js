ce3c81e90d34fe2e98a3d6d29c2c661b2fef2601
Nathan Phelps <nathanaphelps@gmail.com>
2014-09-08 07:09:11 -0500

    added collections.js

---
+++ scripts/collection.js
@@ -0,0 +1,42 @@
+var destinations = [
+
+  {name:"Cave Spring", 
+  	drive: {lat:38.99427826820344, lng: -94.48326040059322},
+  	walk: {lat:38.992703953997214, lng: -94.48306728154415},
+  	password: "grab"},
+
+  {name:"Our First Cache", 
+  	drive: {lat:39.002765625952634, lng:-94.30981341834104}, 
+  	walk: {lat:39.00256222969929, lng:-94.30901948447263}, 
+  	password: "follow"},
+  
+  {name:"KCK",
+  	drive: {lat:39.1189302717068, lng:-94.74898592544366},
+  	walk: {lat:39.120304602581776, lng:-94.74909321380426},
+  	password: "key"},
+
+  {name:"Anna's Oven",
+    drive: {lat:39.056638451594445, lng:-94.60705065443648},
+    walk: {lat:39.0572805723788, lng:-94.60664295866621},
+    password: "dog"},
+
+  {name: "Sonrisa Salon and Day Spa",
+    drive: {lat: 39.2090028174667, lng: -94.65149731779098},
+    walk: {lat: 39.208949912649985, lng: -94.65126798892021},
+    password: "hug"},
+  
+  {name:"H&M", 
+  	drive: {lat:39.04247822403997, lng:-94.59224411045074},
+  	walk: {lat:39.0426078528567, lng:-94.59267862831115},
+  	password: "string"},
+  
+  {name:"The Final Surprise",
+  	drive: {lat:39.03279359951246, lng:-94.59280951782569},
+  	walk: {lat:39.03108459957071, lng:-94.59306566878661}
+    password: "upupdowndownleftrightleftrightba"},
+
+  {name: "VML", 
+    drive: {lat:39.120443233244586, lng: -94.58959449800875},
+    walk: {lat:39.12033729027072, lng: -94.59039379629519},
+    password: "desk"}
+];
