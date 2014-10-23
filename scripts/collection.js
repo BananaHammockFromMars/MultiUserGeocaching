7319cf6cc05f41afc8b1010699d7ffe251f6b4f5
McPhelpsius <nathanaphelps@gmail.com>
2014-10-23 10:54:53 -0500

    lightened up the load here a bit, made it more generic

--- scripts/collection.js
+++ scripts/collection.js
@@ -4,10 +4,10 @@ var destinations = [
   	drive: {lat:38.994870302481374, lng: -94.48349643498653},
     walk:  {lat:38.99307085722472, lng: -94.4837968423962},
   	extra: {lat:38.992703953997214, lng: -94.48306728154415},
-    friend: "aleena",
-  	password: "grab",
+    friend: "friend1",
+  	password: "password1",
     personalPanelText: {
-      transition: '<h4>Happy Birthday, Amber!</h4><p>Time to see what your sneaky boyfriend has been up to!</p><p>Press: <span class="pp-icon">&#xf0c9;</span> to see the driving directions<br>Press: <span>&#xf183;</span> when you have parked the car <br>Press: <span class="pp-icon">&#xf1ea;</span> to hide these instructions</p>'
+      transition: '<h4>This is a demo</h4><p>Press: <span class="pp-icon">&#xf0c9;</span> to see the driving directions<br>Press: <span>&#xf183;</span> when you have parked the car <br>Press: <span class="pp-icon">&#xf1ea;</span> to hide these instructions</p><p>Password: password1</p><p>Friend: friend1</p>'
     }
   },
 
@@ -15,10 +15,10 @@ var destinations = [
   	drive: {lat:39.00439572262789, lng:-94.3085822844032}, 
     walk:  {lat:39.00289827536805, lng:-94.30933598513639},
   	extra: {lat:39.002072897186856, lng:-94.30864397521054},
-    friend: "janine",
-  	password: "follow",
+    friend: "friend2",
+  	password: "password2",
     personalPanelText: {
-      transition: '<h4>What is that?</h4><p>Time to look for the key!</p><p>Did you like my extra little surpsrise there? I have been working a long time and a TON of hours to make you the best birthday I could think of. A little hiking, a little mystery, some important friends, and of course, LETTERS!</p><p>I know you love letters...</p><p>When you get this box, do NOT open it up yet. I will let you know when.</p><p>Hint: If you think to yourself "Oops!", press the button that says "Oops!" It may get you back on track.</p>'
+      transition: '<h4>This is THE demo</h4><p>Time to look for the key!</p><p>Hint: If you think to yourself "Oops!", press the button that says "Oops!" It may get you back on track.</p><p>Password: password2</p><p>Friend: friend2</p>'
     }
   },
   
@@ -26,37 +26,37 @@ var destinations = [
   	drive: {lat:39.1189302717068, lng:-94.74898592544366},
   	walk:  {lat:39.1203031311497, lng:-94.7490838260727},
     extra: {lat:39.120334031215165, lng:-94.74898458433915},
-    friend: "leah",
-  	password: "key",
+    friend: "friend3",
+  	password: "handsomedeveloper",
     personalPanelText: {
-      transition: '<h4>That was a nice little Iowa family reunion!</h4><p>You still do not have the key, though...</p><p>While we are visiting places that you and I have gone (e.g. Hiking, Geocaching), you should head on over to that place where you and I have hung out the most. Maybe there is something there.</p>'
+      transition: '<h4>This could be a marketable product</h4><p>You still do not have the key, though...</p><p>Password: handsomedeveloper</p><p>Friend: friend3</p>'
     }
   },
 
   {name:"Anna's Oven",
     drive: {lat:39.056638451594445, lng:-94.60705065443648},
     walk: {lat:39.0572805723788, lng:-94.60664295866621},
-    password: "dog",
+    password: "falconpunch",
     personalPanelText: {
-      transition: '<h4>Time to change things up a bit!</h4><p>No more boxes, but lots more surprises!</p><p>Slow down for a while, though, and go grab some lunch. I think you are going to love it!</p><p>By the way, you need to get out that pair of Jokers and show people your card. That is your ID today to get the rest of the surprises.</p>'
+      transition: '<h4>Time to change things up a bit!</h4><p>Password:falconpunch</p>'
     }
   },
 
   {name: "Sonrisa Salon and Day Spa",
     drive: {lat: 39.2090028174667, lng: -94.65149731779098},
     walk: {lat: 39.208949912649985, lng: -94.65126798892021},
-    password: "hug",
+    password: "ourlittlesecret",
     personalPanelText: {
-      transition: '<h4>Was lunch good? Was it perfect?</h4><p>I have been aiming for perfect today, so I hope that you are getting perfect.</p><p>Now for something we have never done together, and honestly I have never done AT ALL myself.</p><p>Once again, follow the blue line, and just trust the map.</p><p>P.S. I have yet to tell you I love you today, but I do. I love you dearly.</p>'
+      transition: '<h4>You can use this location...</h4><p>if you feel like getting your nails done.</p><p>I will not judge.</p><p>Password: ourlittlesecret</p>'
     }
   },
   
   {name:"H&M", 
   	drive: {lat:39.04247822403997, lng:-94.59224411045074},
   	walk: {lat:39.0426078528567, lng:-94.59267862831115},
-  	password: "string",
+  	password: "lunchbox",
     personalPanelText: {
-      transition: '<h4>Shopping!</h4><p>Have you found the gift cards in the cigar box yet?</p><p>They are both for $75 each, so I hope you find some cool stuff today. Spend some time with your best pals today and just have fun with your purchases (for YOURSELF, that is the law).</p><p>Love you! I bet you look beautiful, and I cannot wait to see you.</p>'
+      transition: '<h4>Shopping on the Plaza</h4><p>Get yourself somethign nice.</p><p>Password: lunchbox</p>'
     }
   },
   
@@ -65,16 +65,7 @@ var destinations = [
   	walk: {lat:39.03108459957071, lng:-94.59306566878661},
     password: "upupdowndownleftrightleftrightba",
     personalPanelText: {
-      transition: '<h4>This is IT!</h4><p>Final surprise.</p><p>No spoilers.</p>'
-    }
-  },
-
-  {name: "VML", 
-    drive: {lat:39.120443233244586, lng: -94.58959449800875},
-    walk: {lat:39.12033729027072, lng: -94.59039379629519},
-    password: "desk",
-    personalPanelText: {
-      transition: ''
+      transition: '<h4>This is where you get off</h4><p>I hope you have enjoyed this demo. I originally used these same coordinates to propose to my girlfriend. She is now my fiancee, so it worked.</p><p>Thanks for checking it out</p>'
     }
   }
 ];
